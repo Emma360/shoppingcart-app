281334d6ed48eb133ebf53122cec44df6f4caa17
Anozie Emmanuel Chukwuebuka <50842909+Emma360@users.noreply.github.com>
2022-12-30 19:53:20 +0100

    made changes to header

--- src/Home.js
+++ src/Home.js
@@ -6,6 +6,7 @@ const Home = () => {
   console.warn(data.productData);
   return (
     <>
+      <h1>Onyedika Fruits Store </h1>
       <h1 className="text-center mt-3"> All Items </h1>
       <section className="py-4 container">
         <div className="row justify-content-center">
